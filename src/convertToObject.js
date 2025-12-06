42cbb4390643fd3ee955cd539fe295d827fd6630
Andriy <andriit35@gmail.com>
2025-12-06 16:15:18 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,13 +6,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const pairs = sourceString.trim().split(';').filter(Boolean);
+  const pairs = sourceString
+    .trim()
+    .split(';')
+    .map((el) => el.trim())
+    .filter(Boolean);
 
   return Object.fromEntries(
     pairs.map((pair) => {
       const [key, value] = pair.split(':', 2);
 
-      return [key.trim(), value ? value.trim() : ''].filter((el) => el !== '');
+      return [key.trim(), value ? value.trim() : ''];
     }),
   );
 }
